44a6a939888de06a15de10df7956364d6bd1fe72
gutee <mgutierrez@tekgenesis.com>
2017-01-31 21:39:59 -0300

    Titeres levels, json, model.

--- Assets/Scripts/Games/TiteresActivity/TiteresActivityModel.cs
+++ Assets/Scripts/Games/TiteresActivity/TiteresActivityModel.cs
@@ -1,16 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Assets.Scripts.Metrics.Model;
+using Assets.Scripts.Common;
+using SimpleJSON;
+using Assets.Scripts.Games;
 
-public class TiteresActivityModel : MonoBehaviour {
+public class TiteresActivityModel : LevelModel {
+	//time is in seconds
+	public const int START_TIME = 60, CORRECT_SCENE_TIME = 15;
+	public static List<string> NAMES = new List<string>{ "INÉS", "PEDRO", "ARTURO", "LUCÍA" };
 
-	// Use this for initialization
-	void Start () {
-		
+	private int currentLvl;
+	List<TiteresLevel> lvls;
+
+	public TiteresActivityModel() {
+		currentLvl = 0;
+		StartLevels();
+		MetricsController.GetController().GameStart();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public bool GameEnded(){
+		return currentLvl == lvls.Count;
+	}
+
+	void StartLevels() {
+		lvls = new List<TiteresLevel>();
+		JSONArray lvlsJson = JSON.Parse(Resources.Load<TextAsset>("Jsons/TiteresActivity/levels").text).AsObject["levels"].AsArray;
+		foreach(JSONNode lvlJson in lvlsJson) {
+			lvls.Add(new TiteresLevel(lvlJson.AsObject));
+		}
+	}
+
+	public TiteresLevel CurrentLvl() {
+		return lvls[currentLvl];
+	}
+
+	public bool HasTime() {
+		return CurrentLvl().HasTime();
+	}
+
+	public void NextLvl(){
+		currentLvl++;
+	}
+
+	public void Correct() {
+		LogAnswer(true);
+	}
+
+	public void Wrong(){
+		LogAnswer(false);
 	}
 }
--- Assets/Scripts/Games/TiteresActivity/TiteresActivityView.cs
+++ Assets/Scripts/Games/TiteresActivity/TiteresActivityView.cs
@@ -1,19 +1,86 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-namespace Assets.Scripts.Games.TiteresActivity
-{
-public class TiteresActivityView : LevelView {
+using UnityEngine.UI;
+using Assets.Scripts.Common;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+namespace Assets.Scripts.Games.TiteresActivity {
+	public class TiteresActivityView : LevelView {
+		public Text clock, rules;
+		public Button next, previous, okBtn, soundBtn;
+
+		public Image obj;
+		public List<Image> draggers;
+
+		public Sprite[] objects, landscapes;
+		public Randomizer objectLandscapeRandomizer;
+
+		private TiteresActivityModel model;
+
+		public void Start(){
+			model = new TiteresActivityModel();
+			objects = Resources.LoadAll<Sprite>("Sprites/TiteresActivity/objects");
+			landscapes = Resources.LoadAll<Sprite>("Sprites/TiteresActivity/landscapes");
+			Begin();
+		}
+
+		public void Begin(){
+			ShowExplanation();
+		}
 
 		override public void Next(bool first = false){
-			//todo
+			if(model.GameEnded()) {
+				EndGame(60, 0, 1250);
+			} else {
+				ResetPuppets();
+				SetCurrentLevel();
+
+				CheckOk();
+			}
 		}
-	
-	
-}
+
+		void SetCurrentLevel() {
+			if(model.HasTime()){
+				TimeLevel(model.CurrentLvl());
+			} else {
+				NormalLevel(model.CurrentLvl());
+			}
+		}
+
+		void NormalLevel(TiteresLevel lvl) {
+			
+		}
+
+		void TimeLevel(TiteresLevel lvl) {
+			
+		}
+
+		void ResetPuppets() {
+			
+		}
+
+		public void NextClick(){
+			
+		}
+
+		public void PreviousClick(){
+
+		}
+
+		public void SoundClick(){
+			
+		}
+
+		public void OkClick(){
+
+		}
+
+		public void CheckOk(){
+			
+		}
+
+		public void RestartGame(){
+			Start();
+		}
+	}
 }
