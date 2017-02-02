2f64a18f080179428bb916b08491466839261eb6
marycraig <marycraig@oxboard.com>
2017-02-02 19:03:51 -0300

    un poco de todo

---
+++ Assets/Scripts/Games/RompecabezasActivity/RompecabezasActivityView.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.Games;
+using UnityEngine;
+namespace Assets.Scripts.Games.RompecabezasActivity {
+public class RompecabezasActivityView : LevelView {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	override public void Next(bool first = false){
+		
+	}
+	
+}
+}
